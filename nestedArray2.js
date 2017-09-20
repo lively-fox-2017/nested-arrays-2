a68bce0a0ce847fa4a7b8353066273013a11df39
kalibani <kalibani.ka@gmail.com>
2017-09-20 19:01:36 +0700

    nestedArray2 done

---
+++ nestedArray2.js
@@ -0,0 +1,23 @@
+let roster = [
+  ['Number', 'Name', 'Position', 'Points per Game'],
+  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
+  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
+  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
+  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
+  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+]
+
+function convert_roster_format (nestedArray) {
+  let arr=[];
+  for(let i=1; i<nestedArray.length; i++){
+  	let object={};
+  	for(let j=0; j<nestedArray[i].length; j++){
+  		object[nestedArray[0][j]]=nestedArray[i][j]
+  	}
+  	arr.push(object);
+  }
+  return arr
+}
+
+let object_roster = convert_roster_format(roster)
+console.log(object_roster[3])
