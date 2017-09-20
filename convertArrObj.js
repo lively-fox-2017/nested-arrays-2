d4f48865c7dd88206bce73d686e8f2b219696d91
azhariemuhammad <azharie_muhammad@live.com>
2017-09-20 19:54:39 +0700

    revisi

--- convertArrObj.js
+++ convertArrObj.js
@@ -5,32 +5,19 @@ var roster = [
     [31, 'Harvey Kay ', 'Shooting Guard', [0, 30, 16, 0, 25]],
     [7, 'Sally Talls', 'Power Forward', [18, 29, 26, 31, 19]],
     [22, 'MK DiBoux', 'Small Forward', [11, 0, 23, 17, 0]],
-  ];
+];
 
-function convert_roster_format(roster){
-    var obj = {}
+function convert_roster_format(roster) {
+
     var arr = []
-    for (var i = 1; i < roster.length; i++){
-        for(var j = 0; j < roster[0].length; j++){
+    for (var i = 1; i < roster.length; i++) {
+        var obj = {}
+        for (var j = 0; j < roster[i].length; j++) {
             debugger
-            if (roster[0][j] === 'Number'){
-                obj.number = roster[i][j]
-            }
-            else if (roster[0][j] === 'Name'){
-                obj.name = roster[i][j]
-            }
-            else if (roster[0][j] === 'Position'){
-                obj.position = roster[i][j]
-            }
-            else if (roster[0][j] === 'Point per Game'){
-                obj.pointsPerGame = roster[i][j]
-            }
+            obj[roster[0][j]] = roster[i][j];
         }
         arr.push(obj)
-        obj = {}
-        
     }
-  
     return arr;
 }
 console.log(convert_roster_format(roster));
