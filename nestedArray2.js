dcf32e6c3df35a85a5acd83dfccf8f75d1fc5328
NNcrawler <rasyid.xyz@gmail.com>
2017-09-20 11:35:27 +0700

    completed Data test Done

--- nestedArray2.js
+++ nestedArray2.js
@@ -22,6 +22,9 @@ function convert_roster_format(roster){
 var roster = [['Number','Name', 'Position', 'Points per Game'],
 [12, 'Joe schmo', 'Center',[14, 32, 7, 6, 23]],
 [9, 'Ms.Buckets', 'Point Guard',[19, 0,11,22,0]],
+[31, 'Harvey Kay', 'Shooting Guard', [0,30, 16, 0,25]],
+[7, 'Sally Talls', 'Power Forward', [18,29,26,31,19]],
+[22, 'Mk DiBoux', 'Small Forward', [11,0,23,17,0]],
 ]
 var object_roster = convert_roster_format(roster);
 console.log(object_roster);
